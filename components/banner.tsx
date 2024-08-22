3d516a8534729d94e9275a977b3ac01504c416b3
shashank <shashank@jeweln.me>
2024-08-22 23:18:42 +0530

    init commit

---
+++ components/banner.tsx
@@ -0,0 +1,7 @@
+export const Banner = () => {
+  return (
+    <div className=" py-4">
+      <h1 className="flex justify-center items-center text-2xl">crossflow</h1>
+    </div>
+  );
+};
---
+++ components/bottomNavigation.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { GoHome } from "react-icons/go";
+import { LuHistory } from "react-icons/lu";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export const BottomNavigation = () => {
+  const path = usePathname();
+
+  return (
+    <div className="grid grid-cols-3 grid-rows-1 absolute bottom-0 left-0 right-0 text-[28px] py-6 place-items-center ">
+      <Link href={"/"}>
+        <GoHome
+          className={`cursor-pointer ${path === "/" && "text-secondary"}`}
+        />
+      </Link>
+
+      <Link href={"/history"}>
+        <LuHistory
+          className={`cursor-pointer ${
+            path === "/history" && "text-secondary"
+          }`}
+        />
+      </Link>
+
+      <Link href={"/account"}>
+        <MdOutlineAccountCircle
+          className={`cursor-pointer ${
+            path === "/account" && "text-secondary"
+          }`}
+        />
+      </Link>
+    </div>
+  );
+};
